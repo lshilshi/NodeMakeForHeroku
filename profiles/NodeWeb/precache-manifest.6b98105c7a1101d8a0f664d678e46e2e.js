6aad630064d695ca1b4bc7632ce2083d47298bdd
GitHub Action <noreply@github.com>
2021-05-16 01:03:45 +0000

    Update NodeWeb from XOS/NodeWeb

---
+++ profiles/NodeWeb/precache-manifest.6b98105c7a1101d8a0f664d678e46e2e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72cd7d7b6923016ad755aed91ed7ed31",
+    "url": "/_redirects"
+  },
+  {
+    "revision": "1f640532cfbc9d7b48b4",
+    "url": "/css/app.6a5dab7f.css"
+  },
+  {
+    "revision": "c7b179a1d720aaae5b6b",
+    "url": "/css/chunk-vendors.c470e980.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "3f3109e8be2cb8c3ef4cc1e96cd2e14a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f640532cfbc9d7b48b4",
+    "url": "/js/app.75ab8816.js"
+  },
+  {
+    "revision": "a499513b617bf89ef23e",
+    "url": "/js/chunk-33b229d7.d4dfd074.js"
+  },
+  {
+    "revision": "c7b179a1d720aaae5b6b",
+    "url": "/js/chunk-vendors.119deac2.js"
+  },
+  {
+    "revision": "417c67dfde0c71bc33e953d4e7d635f8",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
