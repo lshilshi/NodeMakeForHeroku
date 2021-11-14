2c8fe632f510815241e2d01df22e5c9e3e0c349a
GitHub Action <noreply@github.com>
2021-11-14 12:25:23 +0000

    Update NodeWeb from XOS/NodeWeb

--- profiles/NodeWeb/service-worker.js
+++ profiles/NodeWeb/service-worker.js
@@ -11,19 +11,18 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/js/workbox-v4.3.1/workbox-sw.js");
+workbox.setConfig({modulePathPrefix: "/js/workbox-v4.3.1"});
 
 importScripts(
-  "/precache-manifest.ebe6e589f49248a1593da1643531a7d1.js"
+  "/js/precache-manifest.ef784f68fbb1f9f25e762c30c91847f2.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "web"});
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
+workbox.core.skipWaiting();
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -32,3 +31,8 @@ self.addEventListener('message', (event) => {
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/"), {
+  
+  blacklist: [/\/api\//],
+});
