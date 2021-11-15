bf88c88ae5ae427bd40dc12149631de6bc479eb3
GitHub Action <noreply@github.com>
2021-11-15 00:54:28 +0000

    Update NodeWeb from XOS/NodeWeb

--- profiles/NodeWeb/service-worker.js
+++ profiles/NodeWeb/service-worker.js
@@ -15,7 +15,7 @@ importScripts("/js/workbox-v4.3.1/workbox-sw.js");
 workbox.setConfig({modulePathPrefix: "/js/workbox-v4.3.1"});
 
 importScripts(
-  "/js/precache-manifest.ef784f68fbb1f9f25e762c30c91847f2.js"
+  "/js/precache-manifest.a936b13457d2d89e495f5c0c45adf311.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "web"});
